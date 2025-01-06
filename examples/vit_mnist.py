f2f2afe646f4d29ae84d12c25a9d8d055ed86b12
huy.nguyen <huy.nguyen@centralesupelec.fr>
2025-01-06 11:56:28 +0100

    copyrights

--- examples/vit_mnist.py
+++ examples/vit_mnist.py
@@ -1,6 +1,6 @@
 # MIT License
 
-# Copyright (c) 2023 Jérémy Fix
+# Copyright (c) 2023 Jérémy Fix, Xuan-Huy Nguyen
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
