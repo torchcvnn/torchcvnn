bfc1cd0756b2e012f1529dfc368345f0a33faad0
huy.nguyen <huy.nguyen@centralesupelec.fr>
2025-01-10 11:08:44 +0100

    fix: assure all meta values can be easily processed by collate_fn

--- src/torchcvnn/datasets/sample/dataset.py
+++ src/torchcvnn/datasets/sample/dataset.py
@@ -168,6 +168,8 @@ class SAMPLE(Dataset):
             ]
         }
 
+        meta = {k: v[0][0].astype(np.float32) for k, v in meta.items()}
+
         complex_img = data["complex_img"][:, :, np.newaxis]
 
         class_idx = self.class_names.index(key)
