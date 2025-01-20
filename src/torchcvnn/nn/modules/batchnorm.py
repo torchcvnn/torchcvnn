49fead07121106809d0a587c7c0f1f44db47fde4
Chengfang Ren <chengfang.ren@centralesupelec.fr>
2025-01-20 10:08:04 +0100

    add eps in the svd

--- src/torchcvnn/nn/modules/batchnorm.py
+++ src/torchcvnn/nn/modules/batchnorm.py
@@ -29,54 +29,6 @@ import torch.nn as nn
 import torch.nn.init as init
 
 
-
-
-def safe_inverse(x, epsilon=1E-12):
-    return x/(x**2 + epsilon)
-
-class SVD(torch.autograd.Function):
-    #Solving SVD/PCA backward issue 
-    #https://discuss.pytorch.org/t/svt-backward-error-for-low-rank-structure/192397/2
-    @staticmethod
-    def forward(self, A):
-        U, S, V = torch.svd(A)
-        self.save_for_backward(U, S, V)
-        return U, S, V
-
-    @staticmethod
-    def backward(self, dU, dS, dV):
-        U, S, V = self.saved_tensors
-        Vt = V.t()
-        Ut = U.t()
-        M = U.size(0)
-        N = V.size(0)
-        NS = len(S)
-
-        F = (S - S[:, None])
-        F = safe_inverse(F)
-        F.diagonal().fill_(0)
-
-        G = (S + S[:, None])
-        G.diagonal().fill_(np.inf)
-        G = 1/G 
-
-        UdU = Ut @ dU
-        VdV = Vt @ dV
-
-        Su = (F+G)*(UdU-UdU.t())/2
-        Sv = (F-G)*(VdV-VdV.t())/2
-
-        dA = U @ (Su + Sv + torch.diag(dS)) @ Vt 
-        if (M>NS):
-            dA = dA + (torch.eye(M, dtype=dU.dtype, device=dU.device) - U@Ut) @ (dU/S) @ Vt 
-        if (N>NS):
-            dA = dA + (U/S) @ dV.t() @ (torch.eye(N, dtype=dU.dtype, device=dU.device) - V@Vt)
-        return dA
-
-
-def svd_function(input_matrix: torch.Tensor) -> torch.Tensor:
-    return SVD.apply(input_matrix)
-
 def batch_cov(points: torch.Tensor, centered: bool = False) -> torch.Tensor:
     """
     Batched covariance computation
@@ -220,7 +172,7 @@ class _BatchNormNd(nn.Module):
     def __init__(
         self,
         num_features: int,
-        eps: float = 1e-5,
+        eps: float = 1e-8,
         cond_tol: float = 1e3,
         momentum: float = 0.1,
         affine: bool = True,
@@ -332,6 +284,8 @@ class _BatchNormNd(nn.Module):
             covs = self.running_var
 
             matU, Sdiag, matV = torch.svd_lowrank(covs)
+
+            Sdiag[:,0] = Sdiag[:,0] + self.eps
 
             thresh = Sdiag[:,0]/Sdiag[:,1] 
 
