612d1ec28fef3c0dd4bd6719e31b867cb6ff848d
Huy Nguyen <xanhy8k99@gmail.com>
2025-02-07 21:14:05 +0100

    remove previous PolSAR implementation

--- src/torchcvnn/transforms/transforms.py
+++ src/torchcvnn/transforms/transforms.py
@@ -620,49 +620,6 @@ class PolSAR(BaseTransform):
         return super().__call__(x)
 
 
-class PolSARtoTensor:
-    """
-    Transform a PolSAR image into a 3D torch tensor.
-    """
-    #TODO
-    def __call__(self, element: Union[np.ndarray, dict]) -> torch.Tensor:
-        if isinstance(element, np.ndarray):
-            assert len(element.shape) == 3, "Element should be a 3D numpy array"
-            if element.shape[0] == 3:
-                return self._create_tensor(element[0], element[1], element[2])
-            if element.shape[0] == 2:
-                return self._create_tensor(element[0], element[1])
-            elif element.shape[0] == 4:
-                return self._create_tensor(
-                    element[0], (element[1] + element[2]) / 2, element[3]
-                )
-
-        elif isinstance(element, dict):
-            if len(element) == 3:
-                return self._create_tensor(element["HH"], element["HV"], element["VV"])
-            elif len(element) == 2:
-                if "HH" in element:
-                    return self._create_tensor(element["HH"], element["HV"])
-                elif "VV" in element:
-                    return self._create_tensor(element["HV"], element["VV"])
-                else:
-                    raise ValueError(
-                        "Dictionary should contain keys HH, HV, VV or HH, VV"
-                    )
-            elif len(element) == 4:
-                return self._create_tensor(
-                    element["HH"], (element["HV"] + element["VH"]) / 2, element["VV"]
-                )
-        else:
-            raise ValueError("Element should be a numpy array or a dictionary")
-
-    def _create_tensor(self, *channels) -> torch.Tensor:
-        return torch.as_tensor(
-            np.stack(channels, axis=-1).transpose(2, 0, 1),
-            dtype=torch.complex64,
-        )
-
-
 class Unsqueeze(BaseTransform):
     """Add a singleton dimension to the input array/tensor.
 
