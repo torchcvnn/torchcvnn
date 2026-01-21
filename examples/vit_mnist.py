5afd916afd52c4fad0b561e1859663cc31aebce5
Jeremy Fix <jeremy.fix@gmail.com>
2026-01-21 17:32:44 +0100

    clean up main script, moving the mdoel definition in its own script

--- examples/vit_mnist.py
+++ examples/vit_mnist.py
@@ -47,6 +47,7 @@ import torchcvnn.models as c_models
 # Local imports
 import utils
 import vit_huy
+import vit_tcvnn
 
 
 class PseudoNorm(nn.Module):
@@ -56,157 +57,6 @@ class PseudoNorm(nn.Module):
 
     def forward(self, x):
         return x
-
-
-class PatchEmbedder(nn.Module):
-
-    def __init__(
-        self,
-        image_size: Union[int, Tuple[int, int]],
-        cin,
-        hidden_dim,
-        patch_size,
-        norm_layer: nn.Module = c_nn.LayerNorm,
-        device: torch.device = None,
-        dtype=torch.complex64,
-    ):
-        super(PatchEmbedder, self).__init__()
-
-        factory_kwargs = {"device": device, "dtype": dtype}
-
-        if isinstance(image_size, int):
-            image_size = (image_size, image_size)
-
-        self.embedder = nn.Sequential(
-            norm_layer([cin, *image_size], **factory_kwargs),
-            nn.Conv2d(
-                cin,
-                hidden_dim,
-                kernel_size=patch_size,
-                stride=patch_size,
-                **factory_kwargs,
-            ),
-            norm_layer(
-                [hidden_dim, image_size[0] // patch_size, image_size[1] // patch_size],
-                **factory_kwargs,
-            ),
-        )
-
-    def rope_embedding(self, H, W, hidden_dim, device):
-        """
-        Computes and return the 2D rotary positional embedding RoPE from
-
-        "Rotary position embedding for Vision Transformer", Heo et al 2024, ECCV
-
-        Args:
-            H (int): Height of the image
-            W (int): Width of the image
-            hidden_dim (int): Hidden dimension of the model
-
-        Returns:
-            torch.Tensor: Positional embeddings for the patches
-        """
-        # Frequency scale is 10000 in original "Attention is all you need paper"
-        # but downscaled to 100 in RoPE paper
-        frequency_scale = 100
-
-        pos_H = torch.arange(H, dtype=torch.float32, device=device)
-        pos_W = torch.arange(W, dtype=torch.float32, device=device)
-
-        # Compute the positional encoding
-        theta_t = frequency_scale ** (
-            torch.arange(0, hidden_dim, 2, dtype=torch.float32, device=device)
-            / (hidden_dim / 2)
-        )
-
-        # Apply the sine/cosine embedding
-        emb = torch.exp(1j * theta_t)
-
-        # Compute the positional embedding
-        emb_H = emb[:, None] ** pos_H[None, :]  # (hidden_dim//2, H)
-        emb_W = emb[:, None] ** pos_W[None, :]  # (hidden_dim//2, W)
-
-        # The even dimensions of the features use the encoding of the height
-        # while the odd dimensions use the encoding of the width
-        # So that we interleave the emb_H and emb_W
-        embeddings = torch.zeros(hidden_dim, H, W, dtype=torch.complex64, device=device)
-        embeddings[0::2, :, :] = emb_H[:, :, None]
-        embeddings[1::2, :, :] = emb_W[:, None, :]
-
-        return embeddings
-
-    def forward(self, x):
-        patch_embeddings = self.embedder(x)  # (B, embed_dim, num_patch_H, num_patch_W)
-
-        num_patches_H, num_patches_W = patch_embeddings.shape[2:]
-
-        # Adds the positionnal embedding
-        pos_emb = self.rope_embedding(
-            num_patches_H, num_patches_W, patch_embeddings.shape[1], device=x.device
-        )
-        patch_embeddings = patch_embeddings + pos_emb
-
-        return patch_embeddings
-
-
-class Model(nn.Module):
-
-    def __init__(self, opt: dict, num_classes: int):
-        super().__init__()
-
-        patch_size = opt["patch_size"]
-        input_size = opt["input_size"]
-        hidden_dim = opt["hidden_dim"]
-        num_layers = opt["num_layers"]
-        num_heads = opt["num_heads"]
-        num_channels = opt["num_channels"]
-        dropout = opt["dropout"]
-        attention_dropout = opt["attention_dropout"]
-        # norm_layer = opt["norm_layer"]
-
-        # The hidden_dim must be adapted to the hidden_dim of the ViT model
-        # It is used as the output dimension of the patch embedder but must match
-        # the expected hidden dim of your ViT
-        # norm_layer = PseudoNorm
-        norm_layer = c_nn.RMSNorm
-        # norm_layer = c_nn.LayerNorm
-
-        embedder = PatchEmbedder(input_size, 1, hidden_dim, patch_size, norm_layer=norm_layer)
-
-        # For using an off-the shelf ViT model, you can use the following code
-        # If you go this way, do not forget to adapt the hidden_dim above
-        # For vit_t, it is 192
-        # self.backbone = c_models.vit_t(embedder)
-
-        # For a custom ViT model, you can use the following code
-        # If you go this way, do not forget to adapt the hidden_dim above
-        # You can reduce it to 32 for example
-
-        mlp_dim = 4 * hidden_dim
-
-        self.backbone = c_nn.ViT(
-            embedder,
-            num_layers,
-            num_heads,
-            hidden_dim,
-            mlp_dim,
-            dropout=dropout,
-            attention_dropout=attention_dropout,
-            norm_layer=norm_layer,
-        )
-
-        # A Linear decoding head to project on the logits
-        self.head = nn.Sequential(
-            nn.Linear(hidden_dim, num_classes, dtype=torch.complex64)
-        )
-
-    def forward(self, x):
-        features = self.backbone(x)  # B, num_patches, hidden_dim
-
-        # Global average pooling of the patches encoding
-        mean_features = features.mean(dim=1)
-
-        return self.head(mean_features)
 
 
 def train():
@@ -289,11 +139,11 @@ def train():
         "dropout": 0.3,
         "attention_dropout": 0.1,
         # "norm_layer": "rms_norm",
-        "model_type": "hybrid-vit"
+        "model_type": "vit"
     }
 
     ## Our implementation
-    model = Model(opt, num_classes)
+    model = vit_tcvnn.Model(opt, num_classes)
 
     ## Huy implementation
     # model = vit_huy.VisionTransformer(opt, num_classes)
